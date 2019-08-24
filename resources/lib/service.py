6bc884e746f354f0d06081b086ed7c942badbf2d
Vagios Vlachos <bagios234@gmail.com>
2019-08-24 15:36:31 +0300

    Log file name when playing video

--- resources/lib/service.py
+++ resources/lib/service.py
@@ -9,6 +9,8 @@ import xbmcaddon
 
 
 ADDON = xbmcaddon.Addon()
+PLAYER = xbmc.Player()
+
 logger = logging.getLogger(ADDON.getAddonInfo('id'))
 
 
@@ -16,8 +18,18 @@ def run():
     monitor = xbmc.Monitor()
 
     while not monitor.abortRequested():
+        if PLAYER.isPlayingVideo():
+            logger.debug("Player is playing video: %s" %
+                         PLAYER.getPlayingFile())
+            logger.debug("Metadata: %s" % PLAYER.getVideoInfoTag().getTitle())
+            print(PLAYER.getVideoInfoTag())
+            logger.debug("Metadata: %s" %
+                         PLAYER.getVideoInfoTag().getTVShowTitle())
+            logger.debug("Metadata: %s" %
+                         PLAYER.getVideoInfoTag().getEpisode())
+            kodiutils.notification("Kitsu", PLAYER.getPlayingFile())
         # Sleep/wait for abort for 10 seconds
-        if monitor.waitForAbort(10):
+        if monitor.waitForAbort(5):
             # Abort was requested while waiting. We should exit
             break
         logger.debug("hello addon! %s" % time.time())
