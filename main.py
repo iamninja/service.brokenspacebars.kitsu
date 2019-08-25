8a33405f273eacfbc9216ef22e2720b2b80d536a
Vagios Vlachos <bagios234@gmail.com>
2019-08-25 03:20:08 +0300

    Add igorcmoura/anitopy submodule

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 from resources.lib import kodilogging
 from resources.lib import service
+from resources.modules import anitopy
 
 import logging
 import xbmcaddon
@@ -9,6 +10,8 @@ import xbmcaddon
 # Keep this file to a minimum, as Kodi
 # doesn't keep a compiled copy of this
 ADDON = xbmcaddon.Addon()
+PLAYER = xbmc.Player()
+
 kodilogging.config()
 
-service.run()
+service.KitsuService(PLAYER)
--- resources/lib/service.py
+++ resources/lib/service.py
@@ -2,6 +2,8 @@
 
 from resources.lib import kodiutils
 from resources.lib import kodilogging
+from resources.modules.anitopy import anitopy
+
 import logging
 import time
 import xbmc
@@ -9,27 +11,29 @@ import xbmcaddon
 
 
 ADDON = xbmcaddon.Addon()
-PLAYER = xbmc.Player()
-
 logger = logging.getLogger(ADDON.getAddonInfo('id'))
 
 
-def run():
-    monitor = xbmc.Monitor()
+class KitsuService():
 
-    while not monitor.abortRequested():
-        if PLAYER.isPlayingVideo():
-            logger.debug("Player is playing video: %s" %
-                         PLAYER.getPlayingFile())
-            logger.debug("Metadata: %s" % PLAYER.getVideoInfoTag().getTitle())
-            print(PLAYER.getVideoInfoTag())
-            logger.debug("Metadata: %s" %
-                         PLAYER.getVideoInfoTag().getTVShowTitle())
-            logger.debug("Metadata: %s" %
-                         PLAYER.getVideoInfoTag().getEpisode())
-            kodiutils.notification("Kitsu", PLAYER.getPlayingFile())
-        # Sleep/wait for abort for 10 seconds
-        if monitor.waitForAbort(5):
-            # Abort was requested while waiting. We should exit
-            break
-        logger.debug("hello addon! %s" % time.time())
+    def __init__(self, player):
+        self.player = player
+        self.run()
+
+    def run(self):
+        monitor = xbmc.Monitor()
+
+        while not monitor.abortRequested():
+            if self.player.isPlayingVideo():
+                logger.debug("Player is playing video: %s" %
+                             self.player.getPlayingFile())
+                filename = xbmc.getInfoLabel('Player.Filename')
+                logger.debug("filename: %s" % filename)
+                kodiutils.notification("Kitsu", filename)
+                data = anitopy.parse(filename)
+                logger.debug(data)
+            # Sleep/wait for abort for 10 seconds
+            if monitor.waitForAbort(5):
+                # Abort was requested while waiting. We should exit
+                break
+            logger.debug("hello addon! %s" % time.time())
